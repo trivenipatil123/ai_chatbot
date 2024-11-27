4e70e91d8a686f17679dc61e22305fe452141f9e
triveni-patil-katapult <https://triveni-patil-katapult@github.com>
2024-11-27 12:07:42 +0530

    Removed sensetive data

--- app/chatbot.py
+++ app/chatbot.py
@@ -1,9 +1,9 @@
 import os
 import openai
 
-# openai_api_key = os.getenv("OPENAI_API_KEY")
+openai_api_key = os.getenv("OPENAI_API_KEY")
 # openai_api_key = "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"
-openai_api_key = "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"
+# openai_api_key = "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"
 openai.api_key = openai_api_key
 
 
